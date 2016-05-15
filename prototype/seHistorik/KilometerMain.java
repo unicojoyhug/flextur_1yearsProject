bfc4b8bdfcac7a1b3328e2ee6a2bdd5c5093b217
Jonas Mørch <Jonas Mrch@172.0.0.141>
2016-05-15 21:41:31 +0200

    Kilometer Udregning vha. Google maps

---
+++ prototype/seHistorik/KilometerMain.java
@@ -0,0 +1,20 @@
+package seHistorik;
+
+import java.io.IOException;
+
+import javax.xml.xpath.XPathExpressionException;
+
+public class KilometerMain {
+	
+	private static String Origin = "Stadilvej 14, 6980";
+	private static String Destination = "Munkgaards Alle 88, 7400";
+	private static KilometerUdregning KU = new KilometerUdregning();
+	
+	
+
+	public static void main(String[] args) throws XPathExpressionException, IOException {
+		KU.Distance(Origin, Destination);
+		
+	}
+
+}
---
+++ prototype/seHistorik/KilometerUdregning.java
@@ -0,0 +1,50 @@
+package seHistorik;
+
+import java.io.IOException;
+import java.io.StringReader;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+public class KilometerUdregning {
+
+	private static final String API_KEY = "AIzaSyBoOejgRYqOuDSldGnIDetXOEthJc-CdoM";
+	OkHttpClient client = new OkHttpClient();
+
+	public String Distance(String Origin, String Destination) throws IOException, XPathExpressionException {
+		KilometerUdregning request = new KilometerUdregning();
+		String url_request = "https://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + Origin
+				+ "&destinations=" + Destination + "&mode=driving&language=da-DK&key=" + API_KEY;
+
+		String response = request.run(url_request);
+		String distance = XMLparse(response);
+		return distance;
+	}
+
+	public String run(String url) throws IOException {
+		Request request = new Request.Builder().url(url).build();
+
+		Response response = client.newCall(request).execute();
+		return response.body().string();
+	}
+
+	private String XMLparse(String xml) throws XPathExpressionException {
+
+		XPathFactory xpathFactory = XPathFactory.newInstance();
+		XPath xpath = xpathFactory.newXPath();
+
+		InputSource source = new InputSource(new StringReader(xml));
+		Document doc = (Document) xpath.evaluate("/", source, XPathConstants.NODE);
+		String distance = xpath.evaluate("/DistanceMatrixResponse/row/element/distance/text", doc);
+
+		System.out.println("Distance er =" + distance);
+
+		return distance;
+	}
+}
