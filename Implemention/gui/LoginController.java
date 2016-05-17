0d2c97b19bac66783d71ff75eaf8f6eae7e086c4
Shadowsilver <Shadowsilver@Lantea-Alpha>
2016-05-17 22:20:48 +0200

    Nothing to see here, move along now :)

--- Implemention/gui/LoginController.java
+++ Implemention/gui/LoginController.java
@@ -1,7 +1,38 @@
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class LoginController {
+/**
+ * FXML Controller class
+ *
+ * @author Shadowsilver
+ */
+public class LoginController implements Initializable {
 
+	@FXML
+	private PasswordField password;
+	@FXML
+	private TextField username, ShowPassword;
+	@FXML
+	private CheckBox checkBox;
+		
+	private void ShowPassword() {
+		ShowPassword.managedProperty().bind(checkBox.selectedProperty());
+		ShowPassword.visibleProperty().bind(checkBox.selectedProperty());
+		password.managedProperty().bind(checkBox.selectedProperty().not());
+		password.visibleProperty().bind(checkBox.selectedProperty().not());
+		ShowPassword.textProperty().bindBidirectional(password.textProperty());	
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		ShowPassword();
+			}
 }
