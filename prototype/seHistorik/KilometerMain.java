b5f9f48f86588afb97ed83e44bedc15652215dfb
Juyoung Choi <Juyoung Choi@LAPTOP-QIKL91OU.ceuherning.int>
2016-05-17 12:51:09 +0200

    txt til sql

--- prototype/seHistorik/KilometerMain.java
+++ prototype/seHistorik/KilometerMain.java
@@ -6,7 +6,9 @@ import javax.xml.xpath.XPathExpressionException;
 public class KilometerMain {
 	
 	private static String Origin = "Stadilvej 14, 6980";
-	private static String Destination = "Munkgaards Alle 88, 7400";
+	private static String Destination = "Munkgårds Alle 88, 7400";
+//	private static String Destination = "Rome";
+
 	private static KilometerUdregning KU = new KilometerUdregning();
 	
 	
