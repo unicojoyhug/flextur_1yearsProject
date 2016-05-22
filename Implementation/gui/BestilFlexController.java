30eae91ae8215c8d95e208736782a60075ac23c6
Shadowsilver <Shadowsilver@Lantea-Alpha>
2016-05-22 16:42:40 +0200

    UC2

--- Implementation/gui/BestilFlexController.java
+++ Implementation/gui/BestilFlexController.java
@@ -7,16 +7,24 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Spinner;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import sats.Sats;
+import sats.UnknownKommuneException;
 import util.KilometerUdregning;
 
 /**
@@ -26,22 +34,20 @@ import util.KilometerUdregning;
 public class BestilFlexController implements Initializable {
 
 	@FXML
-	private ChoiceBox<?> fraKommune, tilKommune;
-	@FXML
-	private Spinner<?> personer, barnevogne, koerestole, baggage, autostole;
-	@FXML
-	private Spinner<?> tidspunkt;
+	private ChoiceBox<String> fraKommune, tilKommune;
 	@FXML
 	private ProgressBar prisBar;
 	@FXML
 	private TextArea kommentarer;
 	@FXML
-	private TextField fraAddresse, tilAddresse, pris, PostnrO, PostnrD, kilometer, forventetTid;
+	private TextField fraAddresse, tilAddresse, prisfelt, PostnrO, PostnrD, kilometer, forventetTid, personer, barnevogne,
+			koerestole, baggage, autostole, tidspunkt;
 	@FXML
 	private DatePicker dato;
 	private FlexturGUI flextur;
 	private String seperator = " , ";
 	private KilometerUdregning KU = new KilometerUdregning();
+	private int day, month, year;
 
 	@FXML
 	private void handleBeregnKM(ActionEvent event) throws Throwable, IOException {
@@ -63,15 +69,24 @@ public class BestilFlexController implements Initializable {
 	}
 
 	@FXML
-	private void handleBeregnPris(ActionEvent event) {
+	private void handleBeregnPris(ActionEvent event) throws UnknownKommuneException {
 		prisBar.setVisible(true);
-		String distance = kilometer.getText();
-		String OriginKommune = fraKommune.getConverter().toString();
-		String DestinationKommune = tilKommune.getConverter().toString();
-		// TODO String pris = someone.udregn(distance, OriginKommune,
-		// DestinationKommune);
-		// TODO pris.setText(pris);
+		setDato();
+		
+	//	String distance = kilometer.getText();
+		String OriginKommune = fraKommune.getValue();
+		String DestinationKommune = tilKommune.getValue();
+		double pris = Sats.i().getSats(OriginKommune, DestinationKommune, year, month, day);
+		prisfelt.setText(String.valueOf(pris));
 		prisBar.setVisible(false);
+	}
+
+	private void setDato() {
+		LocalDate localDate = dato.getValue();
+		year = localDate.getYear();
+		month = localDate.getMonthValue();
+		day = localDate.getDayOfMonth();
+
 	}
 
 	@FXML
@@ -81,7 +96,7 @@ public class BestilFlexController implements Initializable {
 		String distance = kilometer.getText();
 		String OriginKommune = fraKommune.getConverter().toString();
 		String DestinationKommune = tilKommune.getConverter().toString();
-		String Pris = pris.getText();
+	//	String Pris = pris.getText();
 		// TODO Finish method
 
 	}
@@ -99,6 +114,11 @@ public class BestilFlexController implements Initializable {
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		prisBar.setVisible(false);
+		fraKommune.setItems(FXCollections.observableArrayList(Sats.i().getKommuner()));
+		fraKommune.getSelectionModel().selectFirst();
+		tilKommune.setItems(FXCollections.observableArrayList(Sats.i().getKommuner()));
+		tilKommune.getSelectionModel().selectFirst();
+
 	}
 
 }
