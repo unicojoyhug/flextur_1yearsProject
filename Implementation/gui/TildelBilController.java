9fd952ecf2c2784b9236c9d6658bdcbaac8e7808
Shadowsilver <Shadowsilver@Lantea-Alpha>
2016-05-27 10:12:48 +0200

    TildelBil gui startet.

---
+++ Implementation/gui/TildelBilController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import logic.Observable;
+import logic.Tilstand;
+
+/**
+ *
+ * @author Jonas Mørch
+ */
+public class TildelBilController extends FSPane implements Initializable {
+    
+    @FXML
+    private FlexturGUI flexturGUI;
+    @FXML
+    private TextField turPersoner, turAutostole, turBarnevogne, turKoerestole, turBaggage, bilPersoner, bilAutostole, bilBarnevogne, bilKoerestole, bilBaggage;
+    @FXML
+    private ComboBox<String> bilValg;
+	
+    
+	
+	public void setMainApp(FlexturGUI flexturGUI) {
+		this.flexturGUI = flexturGUI;
+	}
+	
+    @FXML
+    private void handleGodkend(ActionEvent event) {
+    	
+    }
+    @FXML
+    private void handleAnnuller(ActionEvent event) {
+    	
+    }
+ 
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+	@Override
+	public void update(Observable observable, Tilstand tilstand) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	void postInitialize() {
+		
+	}    
+    
+}
