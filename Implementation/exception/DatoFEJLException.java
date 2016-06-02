fe3807eba62bdb39a21278c209e62b12308fae54
Juyoung Choi <Juyoung Choi@LAPTOP-QIKL91OU.ceuherning.int>
2016-06-02 08:41:18 +0200

    datofejl exception

---
+++ Implementation/exception/DatoFEJLException.java
@@ -0,0 +1,16 @@
+package exception;
+/**
+ * 
+ * @author Juyoung Choi
+ *
+ */
+public class DatoFEJLException extends RuntimeException  {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatoFEJLException(String message) {
+		super(message);
+
+	}
+
+}
